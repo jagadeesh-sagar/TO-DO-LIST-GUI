064eff5bcac24b31b87aa04e29dd165be908bfed
jagadeesh-sagar <nagarajug176@gmail.com>
2024-03-13 23:14:06 +0530

    completed add logic

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import tkinter as tk
+def add2(entry):
+    r=entry.get()
+    list1.append(r)
+    listbox.insert(tk.END,r)
+    entry.delete(0,tk.END)
+def insert():
+    entry=tk.Entry(root,width=10)
+    entry.pack()
+    button1=tk.Button(root,text="enter",command=lambda k=entry:add2(k))
+    button1.pack()
+def add():
+    insert()
 root=tk.Tk()
 root.title("to do list in gui")
 root.geometry("1000x700")
-listbox=tk.Listbox(root,height=5)
+listbox=tk.Listbox(root,height=8)
 listbox.pack()
 list1=['Wake up early','breakfast','jogging','bathing']
+for i in list1:
+    listbox.insert(tk.END,i)
+button1=tk.Button(root,text="ADD",command=add)
+button1.pack()
 
 root.mainloop()
 
